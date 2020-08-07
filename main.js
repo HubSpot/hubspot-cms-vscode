4629f6c4fb8d6de81136304cf2af9edfdaba2b8d
Anthony Pizzurro <apizzurro@hubspot.com>
2020-08-07 06:19:57 -0400

    add option for overriding colors

--- main.js
+++ main.js
@@ -9,7 +9,36 @@ function activate() {
       decreaseIndentPattern: /.*{%(.*?end).*%}*./,
       increaseIndentPattern: /.*{%(?!.*end).*%}.*/,
     },
-  });
+	});
+
+	const override = {
+		"textMateRules": [
+			{
+				"scope": [
+					"meta.scope.hubl.variable",
+					"variable"
+				],
+				"settings": {
+					"foreground": "#e7ca6b"
+				}
+			}
+		]
+	};
+
+	vscode.workspace.onDidChangeConfiguration(e => {
+		if (e.affectsConfiguration('hubl.overrideSyntaxHighlight')) {
+			let hublConfig = vscode.workspace.getConfiguration('hubl');
+			let editorConfig = vscode.workspace.getConfiguration('editor');
+
+			if (hublConfig.get('overrideSyntaxHighlight') === true){
+				vscode.window.showInformationMessage('Override HubL Syntax On');
+				editorConfig.update('tokenColorCustomizations', override);
+			} else {
+				vscode.window.showInformationMessage('Override HubL Syntax Off');
+				editorConfig.update('tokenColorCustomizations', undefined);
+			}
+		}
+	});
 }
 
 function deactivate() {}
