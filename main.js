abf6430d2b6a3b93459efac37f20fb9930999083
Anthony Pizzurro <apizzurro@hubspot.com>
2020-09-10 20:47:50 -0400

    check files to see if there are hubl tags

--- main.js
+++ main.js
@@ -3,13 +3,41 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate() {
-  vscode.languages.setLanguageConfiguration('html', {
-    indentationRules: {
-      decreaseIndentPattern: /.*{%(.*?end).*%}*./,
-      increaseIndentPattern: /.*{%(?!.*end).*%}.*/,
-    },
-  });
+function activate(context) {
+
+const alertUser = (textDocument, lineNumber) => {
+	const buttonLabel = 'Switch this language to HubL';
+	const languageId = 'hubl';
+
+	vscode.window
+	.showInformationMessage(
+		`HubL detected  on line ${lineNumber}`,
+		buttonLabel
+	)
+	.then((selection) => {
+		if (selection === buttonLabel) {
+			vscode.languages.setTextDocumentLanguage(textDocument, 'html');
+		}
+	});
+}
+
+  context.subscriptions.push(
+    vscode.workspace.onDidOpenTextDocument(() => {
+			const textDocument = vscode.window.activeTextEditor.document;
+
+			let n = 1;
+			while (n < 50 && n <= textDocument.lineCount) {
+				const lineWithHubl = textDocument.lineAt(n).text.match(/{{.*}}|{%.*%}/g);
+
+        if (lineWithHubl) {
+
+          alertUser(textDocument, n);
+          break;
+        }
+        n++;
+      }
+    })
+  );
 }
 
 function deactivate() {}
