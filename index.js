c8fb0806b87a398eaa6a3839c14f4c92adb399cd
Anthony Pizzurro <apizzurro@hubspot.com>
2020-12-02 15:43:38 -0500

    POC validate template and add error linting

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const {
   loadConfig,
   validateConfig,
   isTrackingAllowed,
+  getPortalId
 } = require('@hubspot/cms-lib');
 const { trackUsage } = require('@hubspot/cms-lib/api/fileMapper');
+const { validateHubl } = require('@hubspot/cms-lib/api/validate');
 
-async function activate() {
+async function activate(context) {
   const workspaceFolders = vscode.workspace.workspaceFolders;
 
   if (!workspaceFolders || workspaceFolders.length < 1) {
@@ -33,8 +35,51 @@ async function activate() {
   }
 
   await trackUsage('vscode-extension', 'ACTIVATION');
+
+  const collection = vscode.languages.createDiagnosticCollection('hubl');
+
+  if (vscode.window.activeTextEditor) {
+    updateDiagnostics(vscode.window.activeTextEditor.document, collection);
+  }
+
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument((editor) => {
+      if (editor) {
+        updateDiagnostics(editor.document, collection);
+      }
+    })
+  );
 }
 
+const getRange = (document, error) => {
+  if (error.startPosition > 0) {
+    return document.getWordRangeAtPosition(
+      new vscode.Position(error.lineno - 1, error.startPosition - 1)
+    );
+  } else {
+    return document.lineAt(new vscode.Position(error.lineno - 1, 0)).range;
+  }
+};
+
+async function updateDiagnostics(document, collection) {
+  console.log('Request');
+  const { renderingErrors } = await validateHubl(getPortalId(), document.getText());
+
+  const errors = renderingErrors.map((error) => {
+    return {
+      code: '',
+      message: error.message,
+      range: getRange(document, error),
+      severity: vscode.DiagnosticSeverity.Error,
+    };
+  });
+
+  if (document) {
+    collection.set(document.uri, errors);
+  } else {
+    collection.clear();
+  }
+}
 module.exports = {
   activate,
 };
