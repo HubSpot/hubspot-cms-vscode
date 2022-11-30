f4a83cdf237faae49398cc283bc1953aa4585128
Mike Talley <mtalley@hubspot.com>
2022-11-30 15:28:06 -0500

    saving wip

--- src/lib/commands.ts
+++ src/lib/commands.ts
@@ -174,32 +174,37 @@ export const registerCommands = (context: vscode.ExtensionContext) => {
 
   context.subscriptions.push(
     vscode.commands.registerCommand(COMMANDS.INSTALL_HUBSPOT_CLI, () => {
-      vscode.window
-        .showQuickPick(['Install CLI (npm)', 'Install CLI (yarn)'])
-        .then((selection) => {
-          console.log('selection: ', selection);
-        });
+      // vscode.window
+      //   .showQuickPick(['Install CLI (npm)', 'Install CLI (yarn)'])
+      //   .then((selection) => {
+      //     console.log('selection: ', selection);
+      //   });
 
-      // const terminal = vscode.window.createTerminal(TERMINAL_IDS.CLI_INSTALL);
+      const terminal = vscode.window.createTerminal(TERMINAL_IDS.CLI_INSTALL);
 
-      // terminal.show();
-      // vscode.window.onDidCloseTerminal(async (closedTerminal) => {
-      //   if (closedTerminal.name === TERMINAL_IDS.CLI_INSTALL) {
-      //     const retVal = await vscode.commands.executeCommand(
-      //       COMMANDS.CHECK_HUBSPOT_CLI_INSTALL
-      //     );
-      //     console.log('retVal: ', retVal);
-      //     if (retVal) {
-      //       vscode.window.showInformationMessage('HubSpot CLI installed.');
-      //     } else {
-      //       vscode.window.showInformationMessage(
-      //         'Error Installing HubSpot CLI.'
-      //       );
-      //     }
-      //   }
-      // });
-      // terminal.sendText("echo 'Installing the HubSpot CLI.'");
-      // terminal.sendText('npm i -g @hubspot/cli@latest');
+      terminal.show();
+
+      // TODO - Poll for 'which hs' value instead of this
+      vscode.window.onDidCloseTerminal(async (closedTerminal) => {
+        if (closedTerminal.name === TERMINAL_IDS.CLI_INSTALL) {
+          const retVal = await vscode.commands.executeCommand(
+            COMMANDS.CHECK_HUBSPOT_CLI_INSTALL
+          );
+          console.log('retVal: ', retVal);
+          if (retVal) {
+            vscode.window.showInformationMessage('HubSpot CLI installed.');
+          } else {
+            vscode.window.showInformationMessage(
+              'Error Installing HubSpot CLI.'
+            );
+          }
+        }
+      });
+      terminal.sendText("echo 'Installing the HubSpot CLI.'");
+      terminal.sendText('npm i -g @hubspot/cli@latest');
+      terminal.sendText(
+        "echo 'Installation complete. You can now close this terminal window.'"
+      );
       // terminal.sendText('exit');
     })
   );
