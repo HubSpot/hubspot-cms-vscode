dfa98791e1b67ce6df6fde14c024485fba5a619a
Anthony Pizzurro <apizzurro@hubspot.com>
2022-12-02 16:15:26 -0500

    initial spike to see what's possible

--- src/lib/providers.ts
+++ src/lib/providers.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
-
+import * as fs from 'fs';
+import * as path from 'path';
 import { AccountsProvider } from './providers/accountsProvider';
 import { COMMANDS, TREE_DATA } from './constants';
 
@@ -13,4 +14,48 @@ export const initializeProviders = (context: vscode.ExtensionContext) => {
     })
   );
   vscode.window.registerTreeDataProvider(TREE_DATA.ACCOUNTS, accountProvider);
+
+  const filePathHandler = {
+    provideCompletionItems(
+      document: vscode.TextDocument,
+      position: vscode.Position
+    ) {
+      const linePrefix = document
+        .lineAt(position)
+        .text.substr(0, position.character);
+      if (!linePrefix.endsWith("include '")) {
+        return undefined;
+      }
+      const files: vscode.CompletionItem[] = [];
+      try {
+        const filePath = document.uri.fsPath;
+        const fileDir = path.dirname(filePath);
+        fs.readdirSync(fileDir).forEach((file) => {
+          files.push(
+            new vscode.CompletionItem(
+              `./${file}`,
+              vscode.CompletionItemKind.File
+            )
+          );
+        });
+      } catch (e) {
+        console.log('err', e);
+      }
+      return files;
+    },
+    // This is triggered when selecting an option- may not need this
+    //@ts-ignore
+    resolveCompletionItem(item) {
+      console.log(item);
+    },
+  };
+
+  context.subscriptions.push(
+    vscode.languages.registerCompletionItemProvider(
+      'html-hubl',
+      //@ts-ignore
+      filePathHandler,
+      "'"
+    )
+  );
 };
