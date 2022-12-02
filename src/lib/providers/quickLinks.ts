5b8e8e74d107de6ea793c6d70c39a7875eaffce8
Mike Talley <mtalley@hubspot.com>
2022-12-02 11:43:59 -0500

    updated quickLinks

--- src/lib/providers/quickLinks.ts
+++ src/lib/providers/quickLinks.ts
@@ -3,16 +3,26 @@ import { QuickLink } from '../types';
 
 export class QuickLinksProvider implements vscode.TreeDataProvider<QuickLink> {
   getTreeItem(q: QuickLink): vscode.TreeItem {
-    console.log('getting tree item: ', q);
     return new QuickLinkTreeItem(q.label, vscode.Uri.parse(q.url));
   }
 
   getChildren(): Thenable<QuickLink[]> {
-    console.log('getChildren');
     return Promise.resolve([
       {
-        label: 'Foo',
-        url: 'https://www.google.com',
+        label: 'Fetch',
+        url: 'https://developers.hubspot.com/docs/cms/developer-reference/local-development-cli#fetch',
+      },
+      {
+        label: 'Upload',
+        url: 'https://developers.hubspot.com/docs/cms/developer-reference/local-development-cli#upload',
+      },
+      {
+        label: 'Watch',
+        url: 'https://developers.hubspot.com/docs/cms/developer-reference/local-development-cli#watch',
+      },
+      {
+        label: 'Remove',
+        url: 'https://developers.hubspot.com/docs/cms/developer-reference/local-development-cli#remove',
       },
     ]);
   }
@@ -24,10 +34,13 @@ export class QuickLinkTreeItem extends vscode.TreeItem {
     public readonly resourceUri: vscode.Uri
   ) {
     super(label, vscode.TreeItemCollapsibleState.None);
-    this.tooltip = `Open link: ${resourceUri.path}`;
+    this.tooltip = `Open link: ${resourceUri.toString()}`;
     this.iconPath = new vscode.ThemeIcon('link-external');
     this.contextValue = 'link';
-    // this.command =
-    // vscode.env.openExternal(vscode.Uri.parse('https://example.com'));
+    this.command = {
+      command: 'vscode.open',
+      title: '',
+      arguments: [resourceUri],
+    };
   }
 }
