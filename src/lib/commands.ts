ae43e7aa99d707d6c4a9667f80954d42bf3b6b6e
Jessica Sines <jsines@hubspot.com>
2022-12-08 10:00:00 -0500

    Add serverless functions create on right-click

--- src/lib/commands.ts
+++ src/lib/commands.ts
@@ -2,9 +2,11 @@ import * as vscode from 'vscode';
 import { registerCommands as registerConfigCommands } from './commands/config';
 import { registerCommands as registerAuthCommands } from './commands/auth';
 import { registerCommands as registerModuleCommands } from './commands/module';
+import { registerCommands as registerServerlessFunctionCommands } from './commands/serverlessFunction';
 
 export const registerCommands = (context: vscode.ExtensionContext) => {
   registerConfigCommands(context);
   registerAuthCommands(context);
   registerModuleCommands(context);
+  registerServerlessFunctionCommands(context);
 };
--- src/lib/fileHelpers.ts
+++ src/lib/fileHelpers.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 const { createModule } = require('@hubspot/cli-lib/modules');
+const { createFunction } = require('@hubspot/cli-lib/functions');
 
 const copySampleModuleFilesToFolder = (folderPath: string) => {
   return createModule(
@@ -13,6 +14,18 @@ const copySampleModuleFilesToFolder = (folderPath: string) => {
     '',
     folderPath,
     { allowExistingDir: true }
+  );
+};
+
+const copySampleFunctionFilesToFolder = (folderPath: string) => {
+  return createFunction(
+    { 
+      functionsFolder: "Test", 
+      filename: "Example", 
+      endpointPath: "Testing", 
+      endpointMethod: "TEST!" 
+    },
+    folderPath
   );
 };
 
@@ -64,6 +77,67 @@ export const convertFolderToModule = (
 
             vscode.workspace.applyEdit(edit).then(() => {
               copySampleModuleFilesToFolder(uniqueModulePath);
+              resolve(edit);
+            });
+          }
+
+          reject(edit);
+        } catch (e: any) {
+          reject(e);
+        }
+      })
+    );
+  };
+};
+
+/*
+ * Returns a non-duplicate folder name with the format <folderName>.functions
+ * It will add a number to the end of the folder name if it already exists
+ * @param folderName - name of a folder with or without a .functions extension
+ */
+const getUniqueFunctionsFolderName = (folderPath: string) => {
+  const folderName = folderPath.split(path.sep).pop() || '';
+  const hasFunctionsExtension = folderName.split('.').pop() === 'functions';
+  let functionsPath = hasFunctionsExtension ? folderPath : `${folderPath}.functions`;
+  let uniqueFunctionsPath = functionsPath;
+
+  if (!hasFunctionsExtension) {
+    let incrementor = 0;
+    // Add incremental number to module name if it already exists
+    while (fs.existsSync(uniqueFunctionsPath)) {
+      incrementor++;
+      const functionsPathParts = functionsPath.split('.');
+      functionsPathParts.pop();
+      uniqueFunctionsPath = `${functionsPathParts.join('.')}${incrementor}.functions`;
+    }
+  }
+  return uniqueFunctionsPath;
+};
+
+export const convertFolderToServerlessFunction = (
+  folderPath: string,
+  cleanupCallback: Function
+) => {
+  return (e: vscode.FileWillCreateEvent) => {
+    return e.waitUntil(
+      new Promise((resolve, reject) => {
+        try {
+          const edit = new vscode.WorkspaceEdit();
+
+          if (
+            e.files &&
+            e.files.length === 1 &&
+            new RegExp(`${folderPath}/.*`).test(e.files[0].fsPath)
+          ) {
+            cleanupCallback();
+            const uniqueFunctionsFolderPath = getUniqueFunctionsFolderName(
+              e.files[0].fsPath
+            );
+
+            edit.renameFile(e.files[0], vscode.Uri.file(uniqueFunctionsFolderPath));
+
+            vscode.workspace.applyEdit(edit).then(() => {
+              copySampleFunctionFilesToFolder(uniqueFunctionsFolderPath);
               resolve(edit);
             });
           }
