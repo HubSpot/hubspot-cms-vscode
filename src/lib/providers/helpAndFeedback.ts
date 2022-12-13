eee69c59a62ec62743c50566a0a58068de2ebaa6
Mike Talley <mtalley@hubspot.com>
2022-12-13 13:35:42 -0500

    added link to readme

--- src/lib/providers/helpAndFeedback.ts
+++ src/lib/providers/helpAndFeedback.ts
@@ -9,7 +9,7 @@ import { Link } from '../types';
 
 export class HelpAndFeedbackProvider implements TreeDataProvider<any> {
   getTreeItem(q: Link): TreeItem {
-    return new LinkTreeItem(q.label, Uri.parse(q.url));
+    return new UrlLinkTreeItem(q.label, Uri.parse(q.url));
   }
 
   getChildren(): Thenable<any> {
@@ -18,16 +18,20 @@ export class HelpAndFeedbackProvider implements TreeDataProvider<any> {
         label: 'CLI Documentation',
         url: 'https://developers.hubspot.com/docs/cms/developer-reference/local-development-cli#interacting-with-the-developer-file-system',
       },
+      {
+        label: 'About HubSpot VSCode Extension',
+        url: 'https://github.com/HubSpot/hubspot-cms-vscode/blob/master/README.md',
+      },
     ]);
   }
 }
 
-export class LinkTreeItem extends TreeItem {
+export class UrlLinkTreeItem extends TreeItem {
   constructor(public readonly label: string, public readonly resourceUri: Uri) {
     super(label, TreeItemCollapsibleState.None);
     this.tooltip = `Open link: ${resourceUri.toString()}`;
     this.iconPath = new ThemeIcon('link-external');
-    this.contextValue = 'link';
+    this.contextValue = 'url-link-tree-item';
     this.command = {
       command: 'vscode.open',
       title: '',
