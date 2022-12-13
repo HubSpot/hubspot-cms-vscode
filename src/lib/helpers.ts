9867237af6beab96da0d52735c1e67ab64be2a05
Mike Talley <mtalley@hubspot.com>
2022-12-13 14:48:20 -0500

    updated to using path instead of fsPath to help fix issues with urls containing paths in windows

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -9,8 +9,7 @@ export const getRootPath = () => {
   if (!workspaceFolders || workspaceFolders.length < 1) {
     throw new Error('No workspace folder found.');
   }
-
-  return workspaceFolders[0].uri.fsPath;
+  return workspaceFolders[0].uri.path;
 };
 
 export const getDefaultPortalFromConfig = (config: HubspotConfig) => {
