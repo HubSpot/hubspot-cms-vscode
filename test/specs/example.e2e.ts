dd71e14a9d575740e2b0d8b4372ee8b55d843b2f
Jessica Sines <jsines99@gmail.com>
2022-12-22 16:46:34 -0500

    Initial setup for e2e testing

---
+++ test/specs/example.e2e.ts
@@ -0,0 +1,7 @@
+describe('WDIO VSCode Service', () => {
+    it('should be able to load VSCode', async () => {
+        const workbench = await browser.getWorkbench()
+        expect(await workbench.getTitleBar().getTitle())
+            .toBe('[Extension Development Host] Visual Studio Code')
+    })
+})
