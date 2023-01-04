90bce6c31a9561f5efafa3baee5319bc57b4a526
Jessica Sines <jsines99@gmail.com>
2023-01-04 13:17:24 -0500

    Add retries to e2e tests

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,11 +1,13 @@
-describe('WDIO VSCode Service', () => {
+describe('WDIO VSCode Service', function() {
+    this.retries(3);
+
     it('should be able to load VSCode', async () => {
         const workbench = await browser.getWorkbench()
         expect(await workbench.getTitleBar().getTitle())
             .toBe('[Extension Development Host] hubspot-cms-vscode')
     })
     
-    it('should show all activity bar items', async () => {
+    it('extension should appear in sidebar', async () => {
         const workbench = await browser.getWorkbench()
         const viewControls = await workbench.getActivityBar().getViewControls()
         expect(await Promise.all(viewControls.map((vc) => vc.getTitle()))).toEqual([
