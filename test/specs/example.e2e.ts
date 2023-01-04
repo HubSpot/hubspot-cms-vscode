d0ae03d7186ae94bbd6ea5a85021770b17208379
Jessica Sines <jsines99@gmail.com>
2023-01-04 11:29:03 -0500

    Add workspace path, max instances to 1, change typescript config, add extra test to see if extension is in sidebar

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -2,6 +2,19 @@ describe('WDIO VSCode Service', () => {
     it('should be able to load VSCode', async () => {
         const workbench = await browser.getWorkbench()
         expect(await workbench.getTitleBar().getTitle())
-            .toBe('[Extension Development Host] Visual Studio Code')
+            .toBe('[Extension Development Host] hubspot-cms-vscode')
+    })
+    
+    it('should show all activity bar items', async () => {
+        const workbench = await browser.getWorkbench()
+        const viewControls = await workbench.getActivityBar().getViewControls()
+        expect(await Promise.all(viewControls.map((vc) => vc.getTitle()))).toEqual([
+            'Explorer',
+            'Search',
+            'Source Control',
+            'Run and Debug',
+            'Extensions',
+            'HubSpot'
+        ])
     })
 })
--- wdio.conf.ts
+++ wdio.conf.ts
@@ -31,7 +31,7 @@ export const config: Options.Testrunner = {
     // will be called from there.
     //
     specs: [
-        './test/specs/**/*.ts'
+        './test/specs/**/*.e2e.ts'
     ],
     // Patterns to exclude.
     exclude: [
@@ -53,7 +53,7 @@ export const config: Options.Testrunner = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 10,
+    maxInstances: 1,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -64,6 +64,7 @@ export const config: Options.Testrunner = {
         browserVersion: 'stable',
         'wdio:vscodeOptions': {
             extensionPath: __dirname,
+            workspacePath: __dirname,
             userSettings: {
                 "editor.fontSize": 14
             }
