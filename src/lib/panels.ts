520efec7744265e4cdae4cb6985a1f4cf6f5a97f
Mike Talley <mtalley@hubspot.com>
2023-01-11 11:43:09 -0500

    removed console log

--- src/lib/panels.ts
+++ src/lib/panels.ts
@@ -6,7 +6,6 @@ import { FeedbackPanel } from './panels/feedback';
 export const initializePanels = (context: ExtensionContext) => {
   context.subscriptions.push(
     commands.registerCommand(COMMANDS.PANELS.OPEN_FEEDBACK_PANEL, () => {
-      console.log('triggered');
       FeedbackPanel.render(context.extensionUri);
     })
   );
