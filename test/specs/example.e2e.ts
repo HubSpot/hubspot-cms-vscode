0c4c56c55b6ac39e5876eeca0ffb2488a588fb17
Mike Talley <mtalley@hubspot.com>
2023-01-23 13:46:55 -0500

    moved browser.pause to config so it only runs once and updated extension title check so it passes regardless of ordering

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,6 +1,4 @@
 describe('WDIO VSCode Service', function () {
-  before(() => browser.pause(3000));
-
   it('should be able to load VSCode', async () => {
     const workbench = await browser.getWorkbench();
     expect(await workbench.getTitleBar().getTitle()).toBe(
@@ -11,13 +9,13 @@ describe('WDIO VSCode Service', function () {
   it('extension should appear in sidebar', async () => {
     const workbench = await browser.getWorkbench();
     const viewControls = await workbench.getActivityBar().getViewControls();
-    expect(await Promise.all(viewControls.map((vc) => vc.getTitle()))).toEqual([
-      'Explorer',
-      'Search',
-      'Source Control',
-      'Run and Debug',
-      'Extensions',
-      'HubSpot',
-    ]);
+    const viewControlTitles = await Promise.all(
+      viewControls.map((vc) => vc.getTitle())
+    );
+
+    const hubspotViewControlExists =
+      viewControlTitles.indexOf('HubSpot') !== -1;
+
+    expect(hubspotViewControlExists).toBe(true);
   });
 });
