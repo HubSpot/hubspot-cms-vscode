cbeabff6103e5880c023d4b2fc043aef29f6d056
Mike Talley <mtalley@hubspot.com>
2023-01-30 14:44:49 -0500

    saving wip

--- test/specs/auth.e2e.ts
+++ test/specs/auth.e2e.ts
@@ -16,41 +16,44 @@ describe('Auth', function () {
       .getSection('ACCOUNTS');
     const visibleItems = await sidebarSections.getVisibleItems();
 
+    // This depends on the content in /test/hubspot.config.yml
     expect(await Promise.all(visibleItems.map((i) => i.getLabel()))).toEqual([
       'hubspot-cms-vscodeE2ETesting - 23876139 ',
     ]);
   });
 
-  // it('loads context menu', async () => {
-  //   // https://github.com/webdriverio-community/wdio-vscode-service/issues/57
-  //   const sidebarSections = await workbench
-  //     .getSideBar()
-  //     .getContent()
-  //     .getSection('ACCOUNTS');
-  //   const visibleItems = await sidebarSections.getVisibleItems();
-  //   // // const hubspotViewControl = await workbench
-  //   // //   .getActivityBar()
-  //   // //   .getViewControl('HubSpot');
-  //   // // const hubspotSidebarView = await hubspotViewControl?.openView();
-  //   // const menuMaybe = await (
-  //   //   await hubspotSidebarView.getVisibleItems()
-  //   // )[0].openContextMenu();
-  //   // const menuMaybe = await visibleItems[0].openContextMenu();
-  //   await visibleItems[0].select();
+  it('loads context menu', async () => {
+    // https://github.com/webdriverio-community/wdio-vscode-service/issues/57
+    const sidebarSections = await workbench
+      .getSideBar()
+      .getContent()
+      .getSection('ACCOUNTS');
+    const visibleItems = await sidebarSections.getVisibleItems();
+    // // const hubspotViewControl = await workbench
+    // //   .getActivityBar()
+    // //   .getViewControl('HubSpot');
+    // // const hubspotSidebarView = await hubspotViewControl?.openView();
+    // const menuMaybe = await (
+    //   await hubspotSidebarView.getVisibleItems()
+    // )[0].openContextMenu();
+    // const menuMaybe = await visibleItems[0].openContextMenu();
+    await visibleItems[0].elem.click({ button: 'right' });
 
-  //   // browser.debug;
+    // browser.debug;
 
-  //   // await browser.pause(5000);
+    // await browser.pause(5000);
 
-  //   // const sidebarSections = await workbench
-  //   //   .getSideBar()
-  //   //   .getContent()
-  //   //   .getSection('ACCOUNTS');
-  //   // const visibleItems = await sidebarSections.getVisibleItems();
-  //   // const visibleItem = visibleItems[0];
+    // const sidebarSections = await workbench
+    //   .getSideBar()
+    //   .getContent()
+    //   .getSection('ACCOUNTS');
+    // const visibleItems = await sidebarSections.getVisibleItems();
+    // const visibleItem = visibleItems[0];
 
-  //   // console.log(await Promise.all(visibleItems.map((i) => i.getLabel())));
+    // console.log(await Promise.all(visibleItems.map((i) => i.getLabel())));
 
-  //   expect(visibleItems[0]).toEqual(1);
-  // });
+    await browser.saveScreenshot('./foo.png');
+
+    expect(visibleItems[0]).toEqual(1);
+  });
 });
