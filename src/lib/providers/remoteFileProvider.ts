6b1a4eda0c2e9d90d1b7726b9a5d7ca499e062b8
Jessica Sines <jsines99@gmail.com>
2023-02-23 13:49:12 -0500

    Prettier

--- src/lib/providers/remoteFileProvider.ts
+++ src/lib/providers/remoteFileProvider.ts
@@ -1,19 +1,14 @@
-import {
-  TextDocumentContentProvider,
-  EventEmitter,
-  Uri
-} from 'vscode';
+import { TextDocumentContentProvider, EventEmitter, Uri } from 'vscode';
 const { download } = require('@hubspot/cli-lib/api/fileMapper');
 const { getPortalId } = require('@hubspot/cli-lib');
 
-export const RemoteFileProvider = new (class implements TextDocumentContentProvider
+export const RemoteFileProvider = new (class
+  implements TextDocumentContentProvider
 {
   onDidChangeEmitter = new EventEmitter<Uri>();
   onDidChange = this.onDidChangeEmitter.event;
 
-  async provideTextDocumentContent(
-    uri: Uri
-  ): Promise<string | undefined> {
+  async provideTextDocumentContent(uri: Uri): Promise<string | undefined> {
     const filepath = uri.toString().split(':')[1];
     try {
       // filepath must be de-encoded since it gets reencoded by download in cli-lib
