5540f042c31358da57defb56bfcb5cd25d52d754
Branden Rodgers <branden@rodgersworld.com>
2025-06-11 15:17:45 -0400

    support cursor in the auth flow

--- src/lib/commands/auth.ts
+++ src/lib/commands/auth.ts
@@ -1,5 +1,5 @@
 import { commands, env, ExtensionContext, Uri } from 'vscode';
-import { COMMANDS } from '../constants';
+import { COMMANDS, IDE_NAMES } from '../constants';
 
 export const registerCommands = (
   context: ExtensionContext,
@@ -7,7 +7,15 @@ export const registerCommands = (
 ) => {
   context.subscriptions.push(
     commands.registerCommand(COMMANDS.AUTHORIZE_ACCOUNT, async () => {
-      const authUrl = `https://app.hubspot.com/l/personal-access-key?vsCodeExtensionRootPath=${rootPath}`;
+      // Detect the IDE type based on env.appName
+      const ideParam =
+        env.appName.toLowerCase() === IDE_NAMES.CURSOR
+          ? IDE_NAMES.CURSOR
+          : IDE_NAMES.VSCODE;
+
+      const authUrl = `https://app.hubspot.com/l/personal-access-key?vsCodeExtensionRootPath=${rootPath}&source=${ideParam}`;
+
+      console.log('authUrl', authUrl);
 
       const callableUri = await env.asExternalUri(Uri.parse(authUrl));
       await env.openExternal(callableUri);
